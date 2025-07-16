3e7b310c74f5ddca618773f12797d884706f61a9
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-16 11:41:02 +0200

    feat: import reset.css file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,13 @@
 import type { Preview } from '@storybook/react-vite'
+// Importazione foglio di stile reset
+import "../src/styles/reset.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
