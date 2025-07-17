cfa0975dd0e4dee8a6d7083f8cb2fd95416adc02
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-17 10:56:05 +0200

    feat: add class to badge component and import Badge.css file

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,6 +1,8 @@
+import "./Badge.css";
+
 // Definizione del componente Badge come Functional Component (React.FC)
 // React.FC => è un tipo fornito da React per tipizzare i componenti funzionali.
 export const Badge: React.FC = () => {
 
-    return <div>Badge</div>
+    return <div className="badge">Badge</div>
 }
