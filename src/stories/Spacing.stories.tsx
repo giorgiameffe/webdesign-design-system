25885eae4abd5b12654d361c47f40135706cc22d
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-17 12:07:34 +0200

    feat: add spacing

--- src/stories/Spacing.stories.tsx
+++ src/stories/Spacing.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 // Importare i tipi necessari da Storybook per definire i metadati e le storie
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -19,6 +20,64 @@ export default meta;
 // Usa `meta` per sapere qual è il componente e quali props accetta.
 type Story = StoryObj<typeof meta>
 
+const SpaceCalc = ({ value }: { value: string }) => {
+
+    const spacing = React.useMemo(() => {
+
+        const bodyStyle = window.getComputedStyle(document.body);
+        return bodyStyle.getPropertyValue(value);
+    }, [value])
+
+    return <span>{spacing}</span>;
+}
+
+const Style: React.FC = () => {
+
+    return (
+        <style>
+            {
+                `dl {
+                font-size: 1rem;
+                border: 1px solid #ccc;
+                display: grid;
+                max-width: calc(550rem/16);
+                grid-template-columns: 1fr 1fr;
+                border-bottom: none;
+                border-radious: 0.5 rem;
+                }
+
+                dt {
+                font-weight: 600;
+                padding: var(--spacing-sm) var(--spacing-md);
+                border-bottom: 1px solid #ccc; 
+                }
+
+                dd {
+                padding: var(--spacing-sm) var(--spacing-md);
+                border-bottom: 1px solid #ccc; 
+                font-family: monospace;
+                }
+                `
+            }
+        </style>
+    )
+}
+
 export const Default: Story = {
-    render: () => <div>Spacing</div>
+    render: () => (
+
+        <>
+            <Style />
+            <dl>
+                {['zero', 'xs', 'sm', 'md', 'lg', 'xl'].map((key) => (
+                    <React.Fragment key={key}>
+                        <dt>{key}</dt>
+                        <dd>
+                            <SpaceCalc value={`--spacing-${key}`} />
+                        </dd>
+                    </React.Fragment>
+                ))}
+            </dl>
+        </>
+    )
 };
