f27955401b0b9e8027a6c96e498a75269ecddd4d
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-06 12:23:14 +0200

    feat: add type GeneraIInputProps and InternalInput

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,31 +5,36 @@
             { label: "Option 2", value: "option2" },
         ]}
     />
+    <Input kind="radio" label="Label" name="name" options={[
+            { label: "Option 1", value: "option1" },
+            { label: "Option 2", value: "option2" },
+        ]}
+    />
 */
 
 import React from "react";
 
 type InputProps = {
     kind: "text" | "email" | "password";
-    label: React.ReactNode;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 type SelectProps = {
     kind: "select";
-    label: React.ReactNode;
     options: { label: string; value: string }[];
     placeholder?: string;
 } & React.SelectHTMLAttributes<HTMLSelectElement>;
 
-export const Input: React.FC<InputProps | SelectProps> = ({ label, id, ...props }) => {
+type GeneralInputProps = (InputProps | SelectProps) & {
+    label: React.ReactNode;
+};
 
-    const defaultId = React.useId() || id;
-
-    return (
-        <>
-            <label htmlFor={defaultId}>{label}</label>
-            {props.kind === "select" ? (
-                <select {...props} id={defaultId}>
+export const InternalInput: React.FC<InputProps | SelectProps> = (
+    props
+) => {
+    switch (props.kind) {
+        case "select":
+            return (
+                <select {...props}>
                     {props.placeholder && (
                         <option value="" disabled selected>
                             {props.placeholder}
@@ -39,12 +44,21 @@ export const Input: React.FC<InputProps | SelectProps> = ({ label, id, ...props 
                         <option key={option.value} value={option.value}>
                             {option.label}
                         </option>
-
                     ))}
                 </select>
-            ) : (
-                <input id={defaultId} type={props.kind} {...props} />
-            )}
+            );
+        default:
+            return <input type={props.kind} {...props} />;
+    }
+};
+
+export const Input: React.FC<GeneralInputProps> = ({ label, id, ...props }) => {
+    const defaultId = React.useId() || id;
+
+    return (
+        <>
+            <label htmlFor={defaultId}>{label}</label>
+            <InternalInput {...props} id={defaultId} />
         </>
     )
-}
+};
