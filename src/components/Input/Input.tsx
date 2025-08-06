ea4356c1f4fb7e8c1496906e9d0cd6e0c165d82f
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-06 11:35:43 +0200

    feat: add SelectProps

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,10 @@
 /*
   <Input kind="text" placeholder="Placeholder" label="Label" name="name" />
+  <Input kind="select" label="Label" name="name" options={[
+            { label: "Option 1", value: "option1" },
+            { label: "Option 2", value: "option2" },
+        ]}
+    />
 */
 
 import React from "react";
@@ -9,14 +14,24 @@ type InputProps = {
     label: React.ReactNode;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input: React.FC<InputProps> = ({ kind, label, id, ...attrs }) => {
+type SelectProps = {
+    kind: "select";
+    label: React.ReactNode;
+    options: { label: React.ReactNode; value: string }[];
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
+
+export const Input: React.FC<InputProps | SelectProps> = ({ label, id, ...props }) => {
 
     const defaultId = React.useId() || id;
 
     return (
         <>
             <label htmlFor={defaultId}>{label}</label>
-            <input id={defaultId} type={kind} {...attrs} />
+            {props.kind === "select" ? (
+                <select {...props} id={defaultId} />
+            ) : (
+                <input id={defaultId} type={props.kind} {...props} />
+            )}
         </>
     )
 }
