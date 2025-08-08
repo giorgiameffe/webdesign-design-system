b2c82e91aadacc4b2ab93d4d87dd2c81ee474727
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-08 10:50:13 +0200

    feat: add border color for invalid email

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -39,7 +39,7 @@ export const Default: Story = {
     render: ({ label }) => (
         <div>
             {/* Input standard con icona */}
-            <Input label={label} kind="text" icon="Book" iconPosition="left" placeholder="Placeholder" />
+            <Input label={label} kind="email" icon="Book" iconPosition="left" placeholder="Placeholder" />
 
             {/* Input di tipo select con opzioni */}
             <Input
